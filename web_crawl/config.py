61f0494daa13a388522fd10fde6c0ca1c0dea9e6
gkshindia <gkshindia@gmail.com>
2019-09-04 14:48:19 +0530

    Refactored utils and config
    Signed-off-by: gkshindia <gkshindia@gmail.com>

--- web_crawl/config.py
+++ web_crawl/config.py
@@ -2,14 +2,12 @@ import logging
 
 
 class BaseConfig:
-    TODAYS_URL = "https://www.wunderground.com/weather/in/"
     WEATHER_URL = "https://weather.com/en-IN/weather"
     logging.basicConfig(level=logging.DEBUG)
     HEADERS = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/76.0.3809.100 Safari/537.36 OPR/63.0.3368.53'}
     TIMEOUT = 5
-    FIFTEEN_DAY_API_BASE_URL = "https://api.weather.com/v3/wx/forecast/daily/15day"
     API_KEY = "d522aa97197fd864d36b418f39ebb323"
     FORMAT = "json"
     LANGUAGE = "en-IN"
